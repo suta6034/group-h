fb5a9db58e9907413db631ebe3f8e9bf138e2b8f
Joni <jongwon.kang@voestalpine.com>
2025-11-11 13:06:20 +0100

    feat: FE setup

---
+++ FE/music-app/src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+</style>
---
+++ FE/music-app/src/components/TheWelcome.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  Welcome to group-H
+</template>
