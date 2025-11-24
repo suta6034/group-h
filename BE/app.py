9118a8b689c070efd24976889f828fdbb89b4ca2
Joni <jongwon.kang@voestalpine.com>
2025-11-24 16:30:47 +0100

    feat: testing GET API

--- BE/app.py
+++ BE/app.py
@@ -1,12 +1,16 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/')
-def home():
-    return "Hello, Python backend!"
+def predict():
+    return [1, 2, 3, 4]
+
+@app.route('/get-data', methods=['GET'])
+def get_data():
+    result = predict()
+    return jsonify({"data": result})
 
 if __name__ == '__main__':
     app.run(debug=True)
--- FE/music-app/src/App.vue
+++ FE/music-app/src/App.vue
@@ -3,17 +3,27 @@ import { ref, onMounted } from 'vue'
 
 import VDataTableDemo from './components/VDataTableDemo.vue'
 
-const message = ref('')
+const data = ref<number[]>([])
+const error = ref('')
 
 onMounted(async () => {
-  const res = await fetch('http://localhost:5000/')
-  message.value = await res.text()
+  try {
+    const res = await fetch('http://localhost:5000/get-data')
+    if (!res.ok) throw new Error('Failed to fetch')
+    data.value = await res.json()
+  } catch (e) {
+    error.value = 'Error fetching data'
+  }
 })
 </script>
 
 <template>
   <div>
-    {{ message }}
+    <h1>Fetched Data from /get-data</h1>
+    <div v-if="error">{{ error }}</div>
+    <ul v-else>
+      <li v-for="item in data" :key="item">{{ item }}</li>
+    </ul>
     <VDataTableDemo />
   </div>
 </template>
